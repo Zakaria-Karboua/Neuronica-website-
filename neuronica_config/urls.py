1e9e6326342478453756bcebb5d7a5641322fa8c
Zakaria23213 <karbouazakaria905@gmail.com>
2026-07-22 21:28:38 +0100

    Add logo and favicon

--- neuronica_config/urls.py
+++ neuronica_config/urls.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import RedirectView
+from django.templatetags.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('favicon.ico', RedirectView.as_view(url=static('favicon.ico'), permanent=True)),
     path('accounts/', include('allauth.urls')),  # Google + GitHub login lives here
     path('', include('accounts.urls')),
     path('focus/', include('focus.urls')),
